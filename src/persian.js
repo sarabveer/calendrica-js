72e046b97789712d0e5c24986a1b310db36c7a0c
Sarabveer Singh <me@sarabveer.me>
2022-01-10 22:56:08 -0500

    refactor(Persian): use arrow functions

--- src/persian.js
+++ src/persian.js
@@ -3,11 +3,9 @@ const { MARCH, gregorianYearFromFixed } = require( './gregorian' )
 const { MEAN_TROPICAL_YEAR, estimatePriorSolarLongitude, midday, solarLongitude } = require( './astronomy' )
 const { hr, next } = require( './general' )
 
-// Type: fixed-date
 // Fixed date of start of the Persian calendar.
 const PERSIAN_EPOCH = fixedFromJulian( 622, MARCH, 19 )
 
-// Type: location
 // Location of Tehran, Iran.
 const TEHRAN = {
   latitude: 35.68,
@@ -16,7 +14,6 @@ const TEHRAN = {
   zone: hr( 3 + 1 / 2 ),
 }
 
-// Type: fixed-date -> moment
 // Universal time of true noon on fixed $date$ in Tehran.
 const middayInTehran = date => midday( date, TEHRAN )
 
@@ -27,31 +24,29 @@ const Persian = class {
 
     static middayInTehran = middayInTehran
 
+    // Set location for midday calculation
     constructor( LOCATION ) {
       this.midday = date => ( LOCATION ? midday( date, LOCATION ) : middayInTehran( date ) )
     }
 
-    // Type: fixed-date -> fixed-date
     // Fixed date of Astronomical Persian New Year on or before fixed date.
-    persianNewYearOnOrBefore( date ) {
+    persianNewYearOnOrBefore = date => {
       const approx = estimatePriorSolarLongitude( 0, this.midday( date ) )
       return next( Math.floor( approx ) - 1, day => (
         solarLongitude( this.midday( day ) ) <= 0 + 2
       ) )
     }
 
-    // Type: persian-date -> fixed-date
     // Fixed date of Astronomical Persian date p-date.
-    fixedFromPersian( year, month, day ) {
+    fixedFromPersian = ( year, month, day ) => {
       const newYear = this.persianNewYearOnOrBefore(
         PERSIAN_EPOCH + 180 + Math.floor( MEAN_TROPICAL_YEAR * ( year > 0 ? year - 1 : year ) ),
       )
       return newYear - 1 + ( month <= 7 ? 31 * ( month - 1 ) : 30 * ( month - 1 ) + 6 ) + day
     }
 
-    // Type: fixed-date -> persian-date
     // Astronomical Persian date (year month day) corresponding to fixed date.
-    persianFromFixed( date ) {
+    persianFromFixed = date => {
       const newYear = this.persianNewYearOnOrBefore( date )
       const y = Math.round( ( newYear - PERSIAN_EPOCH ) / MEAN_TROPICAL_YEAR ) + 1
       const year = y > 0 ? y : y - 1
@@ -63,9 +58,8 @@ const Persian = class {
       return { year, month, day }
     }
 
-    // Type: gregorian-year -> fixed-date
     // Fixed date of Persian New Year (Nowruz) in Gregorian year g-year.
-    nowruz( gYear ) {
+    nowruz = gYear => {
       const persianYear = gYear - gregorianYearFromFixed( PERSIAN_EPOCH ) + 1
       const y = persianYear <= 0 ? persianYear - 1 : persianYear
       return this.fixedFromPersian( y, 1, 1 )
